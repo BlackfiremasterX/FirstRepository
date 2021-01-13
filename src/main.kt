f892fd59454ff97da34c765576b0b8b046f0d5eb
BlackfiremasterX <misharozental@gmail.com>
2021-01-13 11:09:25 +0700

    Ничего не сделали!

---
+++ src/main.kt
@@ -0,0 +1,14 @@
+fun main()//заглавная функция
+{//начало программы
+    var peremennaya = 100 // var - variable - вариативный - изменяемый
+    val znachenie = 200 // val - value - значение - константа - неизменяемый
+
+    print("Значение переменной peremennaya: ")
+    println(peremennaya) //вывод на экран консоли
+    print("Значение переменной znachenie: ")
+    println(znachenie)//вывод на экран консоли
+
+    peremennaya = 300 // изменение значения переменной peremennaya
+    print("Значение переменной peremennaya после изменения: ")
+    println(peremennaya)
+}//конец программы
